31075917c6bd0aaa428bbd3d7fdbd137e6b859f8
Mohd Nadeem <105824474+codewithnadeem14502@users.noreply.github.com>
2024-03-16 07:11:07 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
         username,
         password,
       });
-     setCookies('access-token', token, { secure: true, sameSite: 'none' });
+     setCookies("access-token", respond.data.token);
       const message = respond.data.message;
       if (message == "Login Successfully") {
         // navigate("/");
