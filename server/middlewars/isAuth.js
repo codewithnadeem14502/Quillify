f155bad6bedac1715f32b155523c00a130256d19
Mohd Nadeem <105824474+codewithnadeem14502@users.noreply.github.com>
2024-03-16 09:45:16 +0530

    Update isAuth.js

--- server/middlewars/isAuth.js
+++ server/middlewars/isAuth.js
@@ -1,19 +1,22 @@
 import jwt from "jsonwebtoken";
-
 export const isAuth = async (req, res, next) => {
-  const token = req.cookies["access-token"]; // Assuming your token is stored in a cookie named "access-token"
-
+  const token = req.cookies["access-token"];
+  console.log("Token value :", token);
   if (!token) {
-    return res.status(401).json({ message: "Unauthorized: Missing access token" });
+    return res.json({ message: "The Token Is Missing" });
+  } else {
+    jwt.verify(token, "secret", (error, decoded) => {
+      if (error) {
+        console.error("Token verification error:", error);
+        return res.json({ message: "Token Wrong" });
+      } else {
+        // console.log("Decoded Token:", decoded);
+        req.email = decoded.email;
+        // console.log("Decoded Email:", req.email);
+        req.username = decoded.username;
+        // console.log("Decoded Username:", req.username);
+        next();
+      }
+    });
   }
-  
-  try {
-    const decoded = jwt.verify(token, "your_secret_key_here");
-    req.email = decoded.email;
-    req.username = decoded.username;
-    next();
-  } catch (error) {
-    console.error("Token verification error:", error);
-    return res.status(401).json({ message: "Unauthorized: Invalid token" });
-  }
-};
+}; 
