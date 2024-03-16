f0e2bab80e538231e3f99c8200f638de7357ce5d
Mohd Nadeem <105824474+codewithnadeem14502@users.noreply.github.com>
2024-03-17 03:45:45 +0530

    Added Middleware correctly

--- server/middlewars/isAuth.js
+++ server/middlewars/isAuth.js
@@ -1,25 +1,62 @@
 import jwt from "jsonwebtoken";
-export const isAuth = async (req, res, next) => {
-  const token = req.cookies["access-token"];
-   const cookieHeader = req.headers.cookie;
-   console.log("Cookie Header :", cookieHeader);
-   console.log("cookie req.cookie = ",req.cookies["access-token"]);
-  console.log("Token value :", token);
-  if (!token) {
-    return res.json({ message: "The Token Is Missing" });
-  } else {
-    jwt.verify(token, "secret", (error, decoded) => {
-      if (error) {
-        console.error("Token verification error:", error);
-        return res.json({ message: "Token Wrong" });
+
+const isAuth = async (req, res, next) => {
+  let token = "";
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      // console.log("Token ", token);
+
+      if (!token) {
+        return res.json({ message: "The Token Is Missing" });
       } else {
-        // console.log("Decoded Token:", decoded);
+        const decoded = jwt.verify(token, "secret");
+        // If you need to use the decoded information, you can access it via `decoded`
+        req.id = decoded.id;
         req.email = decoded.email;
-        // console.log("Decoded Email:", req.email);
-        req.username = decoded.username;
-        // console.log("Decoded Username:", req.username);
+        req.Date = decoded.Date;
+        req.pgplantype = decoded.pgplantype;
+
         next();
       }
-    });
+    } catch (error) {
+      res.status(401).json({ message: "Not authorized" });
+    }
   }
-}; 
+
+  if (!token) {
+    return res.status(404).json({ message: "No token, no authorization" });
+  }
+};
+
+export default isAuth;
+
+// import jwt from "jsonwebtoken";
+// export const isAuth = async (req, res, next) => {
+//   const token = req.cookies["access-token"];
+//    const cookieHeader = req.headers.cookie;
+//    console.log("Cookie Header :", cookieHeader);
+//    console.log("cookie req.cookie = ",req.cookies["access-token"]);
+//   console.log("Token value :", token);
+//   if (!token) {
+//     return res.json({ message: "The Token Is Missing" });
+//   } else {
+//     jwt.verify(token, "secret", (error, decoded) => {
+//       if (error) {
+//         console.error("Token verification error:", error);
+//         return res.json({ message: "Token Wrong" });
+//       } else {
+//         // console.log("Decoded Token:", decoded);
+//         req.email = decoded.email;
+//         // console.log("Decoded Email:", req.email);
+//         req.username = decoded.username;
+//         // console.log("Decoded Username:", req.username);
+//         next();
+//       }
+//     });
+//   }
+// }; 
