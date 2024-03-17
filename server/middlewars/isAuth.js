c8465d4665f28f02006bfa9eb5acdc10a888b89f
Mohd Nadeem <105824474+codewithnadeem14502@users.noreply.github.com>
2024-03-17 06:09:08 +0530

    Update isAuth.js

--- server/middlewars/isAuth.js
+++ server/middlewars/isAuth.js
@@ -1,25 +1,57 @@
+// import jwt from "jsonwebtoken";
+
+// export const isAuth = async (req, res, next) => {
+//   let token = "";
+//   // console.log("Is auth is working ");
+
+//   try {
+//     token = req.cookies["access-token"];
+//     console.log("Token ", token);
+
+//     if (!token) {
+//       return res.json({ message: "The Token Is Missing" });
+//     } else {
+//       const decoded = jwt.verify(token, "secret");
+//       // If you need to use the decoded information, you can access it via `decoded`
+//       req.username = decoded.username;
+//       req.email = decoded.email;
+
+//       next();
+//     }
+//   } catch (error) {
+//     res.status(401).json({ message: "Not authorized" });
+//   }
+
+//   if (!token) {
+//     return res.status(404).json({ message: "No token, no authorization" });
+//   }
+// };
 import jwt from "jsonwebtoken";
 
 export const isAuth = async (req, res, next) => {
   let token = "";
-  // console.log("Is auth is working ");
 
-  try {
-    token = req.cookies["access-token"];
-    console.log("Token ", token);
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      console.log("Token ", token);
 
-    if (!token) {
-      return res.json({ message: "The Token Is Missing" });
-    } else {
-      const decoded = jwt.verify(token, "secret");
-      // If you need to use the decoded information, you can access it via `decoded`
-      req.username = decoded.username;
-      req.email = decoded.email;
+      if (!token) {
+        return res.json({ message: "The Token Is Missing" });
+      } else {
+        const decoded = jwt.verify(token, "secret");
+        // If you need to use the decoded information, you can access it via `decoded`
+     req.username = decoded.username;
+     req.email = decoded.email;
 
-      next();
+        next();
+      }
+    } catch (error) {
+      res.status(401).json({ message: "Not authorized" });
     }
-  } catch (error) {
-    res.status(401).json({ message: "Not authorized" });
   }
 
   if (!token) {
@@ -27,22 +59,5 @@ export const isAuth = async (req, res, next) => {
   }
 };
 
-// export const isAuth = (req, res, next) => {
-//   const token = req.cookies["access-token"]; // Assuming you're using cookies for token storage
-//   console.log("Token :", token);
-//   if (!token) {
-//     return res.status(401).json({ message: "Unauthorized: No token provided" });
-//   }
 
-//   try {
-//     console.log("decoding");
-//     const decoded = jwt.verify(token, "secret"); // Make sure to use the same secret key as used for signing
-//     console.log("answer ", decoded);
-//     // Attach the user object to the request for further usage
-//     req.user = decoded;
-
-//     next(); // Move to the next middleware
-//   } catch (error) {
-//     return res.status(401).json({ message: "Unauthorized: Invalid token" });
-//   }
-// };
+ 
