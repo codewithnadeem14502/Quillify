34dcfcd11656a29cf45ab6a7870f9138c48d7536
Mohd Nadeem <105824474+codewithnadeem14502@users.noreply.github.com>
2024-03-22 06:53:00 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -32,6 +32,24 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+});
+app.use((req, res, next) => {
+  res.cookie("sameSiteCookie", "value", { sameSite: "none", secure: true });
+  next();
+});
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/post", postRouter);
 //app.use(isAuth);
